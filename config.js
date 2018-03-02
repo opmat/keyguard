f6d061ae4cd210aca5d446fa34ec346a7e25c6e4
Don Maxito <maxharz@gmail.com>
2018-03-01 21:44:03 -0600

    some basic structure

---
+++ config.js
@@ -0,0 +1,11 @@
+const localConfig = {
+    vaultOrigin: 'http://localhost',
+    walletOrigin: 'http://localhost'
+}
+
+const liveConfig = {
+    vaultOrigin: 'https://vault.nimiq.com',
+    walletOrigin: 'https://wallet.nimiq.com'
+}
+
+export default localConfig;
---
+++ keystore.js
@@ -0,0 +1,32 @@
+import Boruca from '/libraries/boruca-messaging/src/boruca.js';
+import config from './config.js';
+
+class Keystore {
+
+    constructor() {
+        // Check if we run in iframe or were opened by window.open()
+        this._communicationTarget = window.parent || window.opener;
+
+        const that = this;
+
+        this.Proxy = class {
+            getAddresses() {
+                if (that._communicationTarget.origin === config.vaultOrigin) {
+                    // high secure keys
+                    return [123, 352];
+                }
+            }
+        };
+
+
+        if (this._communicationTarget) {
+            this._connect();
+        }
+    }
+
+    async _connect() {
+        await Boruca.proxy(this._communicationTarget, this._communicationTarget.origin, this.Proxy);
+    }
+}
+
+new Keystore();
