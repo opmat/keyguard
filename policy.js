0402b6550a250c12e5a448cafdc1465172dca59c
Sven <sven.buschbeck@gmail.com>
2018-03-05 21:02:12 -0600

    Implemented first policies 'full' and 'spending-limit' -- full is more of a dummy for testing

--- policy.js
+++ policy.js
@@ -1,15 +1,49 @@
 export default class Policy {
-   constructor(name) {
-      this._name = name;
-   }
+    constructor(name) {
+        this.name = name;
+    }
 
-   equals(otherPolicy) {
-       if (!otherPolicy) return false;
+    equals(otherPolicy) {
+        return otherPolicy && this.name === otherPolicy.name;
+    }
 
-       return this._name === otherPolicy._name;
-   }
+    static get(name, ...args) {
+        //return new Policy.predefined[name](args);
+        return new (Policy.predefined[name].bind.apply(Policy.predefined[name], args));
+    }
 
-   static parse(object) {
-       return new Policy(object._name);
-   }
+    static get predefined() { return {
+        full: FullAccess
+    }}
+
+    allows(method, args) {
+        throw "make a policy and overwrite me"
+    }
+
+    needUi(method, args) {
+        throw "make a policy and overwrite me"
+    }
+}
+
+class FullAccess extends Policy {
+    constructor() { super("full"); }
+    allows(method, args) { return true; }
+    needUi(method, args) { return false; }
+}
+
+class SpendingLimit extends Policy {
+    constructor(limit) {
+        super("spending-limit");
+        this.limit = limit;
+    }
+    set spendingLimit(limit) { this._limit = limit; }
+
+    allows(method, args) { return true; }
+    needUi(method, args) {
+        if (method === "sign") {
+            const { sender, recipient, value, fee } = args;
+            return value > this._limit;
+        }
+        return false;
+    }
 }
