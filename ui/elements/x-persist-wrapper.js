a68caedae0b14f4f317c847d9b9f46ce4763d840
Don Maxito <maxharz@gmail.com>
2018-03-14 02:02:25 -0600

    persist: keep route when changing element; unsubscribe after successfull change

--- ui/elements/x-persist-wrapper.js
+++ ui/elements/x-persist-wrapper.js
@@ -3,6 +3,8 @@ import XIdenticon from '/elements/x-identicon/x-identicon.js';
 import XPasswordSetter from '/elements/x-password-setter/x-password-setter.js';
 import store from '../../store/store.js';
 import XPersistAccount from './x-persist-account.js';
+import { clearPersist } from '../../store/accounts.js';
+import { bindActionCreators } from '/libraries/redux/src/index.js';
 
 export default class XPersistWrapper extends XElement {
 
@@ -13,21 +15,34 @@ export default class XPersistWrapper extends XElement {
     }
 
     onCreate() {
-        store.subscribe(() => {
-            const state = store.getState();
-            if (state.accounts.toBePersisted) {
-                const wrappedElement = XPersistAccount.createElement(new Map()
-                        .set('userFriendlyAddress', state.accounts.toBePersisted)
-                );
-                this.$el.textContent = '';
-                this.$el.appendChild(wrappedElement.$el);
-            }
-        });
+        this.actions = bindActionCreators({ clearPersist }, store.dispatch);
 
-    }
+        this._unsubscribe = store.subscribe(() => this._onStateChange(store.getState()));
+   }
 
     children() {
         return [ XIdenticon, XPasswordSetter ];
     }
 
+    async _onStateChange(state) {
+        if (state.accounts.toBePersisted) {
+            this.actions.clearPersist();
+
+            const newAttributes = new Map()
+                .set('userFriendlyAddress', state.accounts.toBePersisted);
+
+            const { xRoute } = this.attributes;
+
+            if (xRoute) newAttributes.set('xRoute', xRoute);
+
+            const wrappedElement = XPersistAccount.createElement(newAttributes);
+            this.$el.textContent = '';
+            wrappedElement.$el.className = 'in';
+            this.$el.parentNode.replaceChild(wrappedElement.$el, this.$el);
+            this._unsubscribe();
+        }
+    }
+
 }
+
+// Idea: listen to mutation event to react to attribute change. Subscribe a mapStateToAttributes
