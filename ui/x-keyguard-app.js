6fc0db4693f4de3671aacfccf206269a9f760fe4
Sven <sven.buschbeck@gmail.com>
2018-03-16 21:05:40 -0600

    Sign page v1 (needs hooking up to state)

--- ui/x-keyguard-app.js
+++ ui/x-keyguard-app.js
@@ -1,11 +1,10 @@
 import XElement from '/libraries/x-element/x-element.js';
-// TODO what to do when the user reloads the page and the state is not initialized again??
-// > persist the state onUnload
-// import XPersistWrapper from './elements/x-persist-wrapper.js';
+// TODO what to do when the user reloads the page and the state is not initialized again?? > persist the state on unload
 import XRouter from '/elements/x-router/x-router.js';
 import XPersistAccount from './elements/x-persist-account.js';
-import XImportFile from './elements/x-import-file.js';
 import XImportWords from './elements/x-import-words.js';
+import XImportFile from './elements/x-import-file.js';
+import XSign from './elements/x-sign.js';
 
 export default class XKeyguardApp extends XElement {
 
@@ -17,15 +16,13 @@ export default class XKeyguardApp extends XElement {
 
     get __tagName() { return 'body' }
 
-    onCreate() { }
-
     html() {
         return `
         <x-router>
           <x-persist-account x-route="persist"></x-persist-account>
           <x-import-words x-route="import-from-words"> Import via mnemonic phrase </x-import-words>
           <x-import-file x-route="import-from-file"> Import via backup file </x-import-file>
-          <main x-route="sign"> Sign aka createTransaction </main>
+          <x-sign x-route="sign"> Sign aka createTransaction </x-sign>
           <main x-route="export"> Export aka backup </main>
           <main x-route="rename"> Rename account </main>
           <main x-route="/">
@@ -39,8 +36,8 @@ export default class XKeyguardApp extends XElement {
     }
 
     children() {
-        return [ XRouter, XPersistAccount, XImportFile, XImportWords ];
+        return [ XRouter, XPersistAccount, XImportWords, XImportFile, XSign ];
     }
 }
 
-// TODO catch errors in a top level error panel catching all previously uncaught exceptions
+// TODO catch errors in a top level error panel catching all previously uncaught exceptions > XApp
