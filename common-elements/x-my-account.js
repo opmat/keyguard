15c4f72fc0724c2e6541e8485df65a34041837d4
Don Maxito <maxharz@gmail.com>
2018-03-23 10:24:02 -0600

    remove unused parameter

--- common-elements/x-my-account.js
+++ common-elements/x-my-account.js
@@ -2,7 +2,7 @@ import MixinRedux from '/elements/mixin-redux/mixin-redux.js';
 import XAccount from './x-account.js';
 
 export default class XMyAccount extends MixinRedux(XAccount) {
-    static mapStateToProps(state, props) {
+    static mapStateToProps(state) {
         return {
             ...state.request.data
         }
