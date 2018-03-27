bf38f069bd09a8c696bc7b12f1fb40b68eda60eb
Don Maxito <maxharz@gmail.com>
2018-03-27 15:43:39 -0600

    recursive route for password confirmation

--- common-elements/x-set-passphrase.js
+++ common-elements/x-set-passphrase.js
@@ -1,4 +1,5 @@
 import XElement from '/libraries/x-element/x-element.js';
+import XRouter from '/elements/x-router/x-router.js';
 import XMyAccount from '/libraries/keyguard/common-elements/x-my-account.js';
 import XAddress from '/elements/x-address/x-address.js';
 import XPasswordSetter from '/elements/x-password-setter/x-password-setter.js';
@@ -6,11 +7,13 @@ import XPassphraseTipps from '/elements/x-passphrase-tipps/x-passphrase-tipps.js
 
 export default class XSetPassphrase extends XElement {
 
-    html() { return `
+    html() {
+        return `
         <x-my-account></x-my-account>
         <h1>Enter your Passphrase</h1>
         <x-passphrase-tipps></x-passphrase-tipps>
-        <x-password-setter button-label="Confirm" show-indicator="true"></x-password-setter>
+        <x-password-setter x-route="" button-label="Confirm" show-indicator="true"></x-password-setter>
+        <x-password-getter x-route="confirm"></x-password-getter>
         `;
     }
 
@@ -20,7 +23,8 @@ export default class XSetPassphrase extends XElement {
 
     listeners() {
         return {
-            'x-password-setter-submitted': (passphrase) => this.fire('x-set-passphrase', passphrase)
+            'x-password-setter-submitted': () => XRouter.root.goTo(this.$el, '/confirm'),
+            'x-password-getter-submitted': (passphrase) => this.fire('x-set-passphrase', passphrase)
         }
     }
 }
