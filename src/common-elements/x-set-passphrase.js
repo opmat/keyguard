52631850a4c87652ae041c5dc282d68cbb52b730
Don Maxito <maxharz@gmail.com>
2018-03-30 07:54:01 -0600

    working recursive routing

--- src/common-elements/x-set-passphrase.js
+++ src/common-elements/x-set-passphrase.js
@@ -2,6 +2,7 @@ import XElement from '/libraries/x-element/x-element.js';
 import XRouter from '/secure-elements/x-router/x-router.js';
 import XMyAccount from '/libraries/keyguard/src/common-elements/x-my-account.js';
 import XPassphraseSetter from '/secure-elements/x-passphrase-setter/x-passphrase-setter.js';
+import XPassphraseGetter from '/secure-elements/x-passphrase-getter/x-passphrase-getter.js';
 import XPassphraseTipps from '/secure-elements/x-passphrase-tipps/x-passphrase-tipps.js';
 
 export default class XSetPassphrase extends XElement {
@@ -11,20 +12,19 @@ export default class XSetPassphrase extends XElement {
         <h2>Please enter a passphrase to secure your account.</h2>
         <x-my-account></x-my-account>
         <x-passphrase-tipps></x-passphrase-tipps>
-        <x-passphrase-setter button-label="Confirm" show-indicator="true"></x-passphrase-setter>
+        <x-passphrase-setter x-route="" button-label="Confirm" show-indicator="true"></x-passphrase-setter>
         <x-passphrase-getter x-route="confirm"></x-passphrase-getter>
         `;
     }
 
     children() {
-        return [ XPassphraseTipps, XPassphraseSetter, XMyAccount ];
+        return [ XPassphraseTipps, XPassphraseSetter, XPassphraseGetter, XMyAccount ];
     }
 
     listeners() {
         return {
-            /*'x-passphrase-setter-submitted': async () => (await XRouter.instance).goTo(this.$el, '/confirm'),
-            'x-passphrase-getter-submitted': (passphrase) => this.fire('x-set-passphrase', passphrase)*/
-            'x-passphrase-setter-submitted': (passphrase) => this.fire('x-set-passphrase', passphrase)
+            'x-passphrase-setter-submitted': async () => (await XRouter.instance).goTo(this, 'confirm'),
+            'x-passphrase-getter-submitted': (passphrase) => this.fire('x-set-passphrase', passphrase),
         }
     }
 
