c0eaee16a81d0193dff86ff87ad97beaad53b875
Don Maxito <maxharz@gmail.com>
2018-03-30 16:27:56 -0600

    choose config according to host name

--- src/config.js
+++ src/config.js
@@ -1,15 +1,28 @@
-const devConfig = {
-    safeOrigin: location.origin,
-    walletOrigin: location.origin
+function getConfig(host) {
+    switch (host) {
+        case 'https://secure.nimiq.com':
+            return {
+                safeOrigin: 'https://safe.nimiq.com',
+                walletOrigin: 'https://wallet.nimiq.com',
+                mode: 'live'
+            };
+
+        case 'https://secure.nimiq-testnet.com':
+            return {
+                safeOrigin: 'https://safe.nimiq-testnet.com',
+                walletOrigin: 'https://wallet.nimiq-testnet.com',
+                mode: 'test'
+            };
+
+        default:
+            return {
+                safeOrigin: location.origin,
+                walletOrigin: location.origin,
+                mode: 'test'
+            }
+    }
 }
 
-const localConfig = {
+const host = window.location.hostname;
 
-}
-
-const liveConfig = {
-    safeOrigin: 'https://safe.nimiq.com',
-    walletOrigin: 'https://wallet.nimiq.com'
-}
-
-export default devConfig;
+export default getConfig(host);
