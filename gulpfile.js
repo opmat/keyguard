7ba52120963350abe6afeceac5498c9438cd1a93
danimoh <github.danimoh@neverbox.com>
2018-04-01 15:05:40 -0600

    adapt to new base gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,12 @@ const gulp = require('gulp');
 
 const NimiqBuild = require('../../meta/build-process/nimiq-base-gulpfile.js');
 
-gulp.task('build', () => NimiqBuild.build(
-    'src/keyguard.js',
-    'src/keyguard.css',
-    'src/index.html',
-    [],
-    `${__dirname}/../../`,
-    'dist'
-));
+gulp.task('build', () => NimiqBuild.build({
+    jsEntry: 'src/keyguard.js',
+    cssEntry: 'src/keyguard.css',
+    htmlEntry: 'src/index.html',
+    rootPath: `${__dirname}/../../`,
+    distPath: 'dist'
+}));
 
 gulp.task('default', ['build']);
