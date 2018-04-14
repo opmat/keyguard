efb4fe67a1149b728467fd0def2cf54d3b3092e8
Don Maxito <maxharz@gmail.com>
2018-04-14 16:16:38 -0600

    build process: build dist and dist-test versions with different cdns

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,17 @@ const gulp = require('gulp');
 const NimiqBuild = require('../../meta/build-process/nimiq-base-gulpfile.js');
 
 gulp.task('clean', () => NimiqBuild.cleanBuild('deployment-keyguard/dist'));
+gulp.task('clean-test', () => NimiqBuild.cleanBuild('deployment-keyguard/dist-test'));
+
+const replacementsForRelease = [['https://cdn.nimiq-testnet.com', 'https://cdn.nimiq.com']];
 
 gulp.task('build', ['clean'], () => NimiqBuild.build({
     jsEntry: 'src/keyguard.js',
     cssEntry: 'src/keyguard.css',
     htmlEntry: 'src/index.html',
     rootPath: `${__dirname}/../../`,
-    distPath: 'deployment-keyguard/dist'
+    distPath: 'deployment-keyguard/dist',
+    replaceHTMLStrings: replacementsForRelease
 }));
 
 gulp.task('build-iframe', ['build'], () => NimiqBuild.build({
@@ -17,7 +21,24 @@ gulp.task('build-iframe', ['build'], () => NimiqBuild.build({
     cssEntry: 'src/keyguard.css',
     htmlEntry: 'src/iframe.html',
     rootPath: `${__dirname}/../../`,
-    distPath: 'deployment-keyguard/dist'
+    distPath: 'deployment-keyguard/dist',
+    replaceHTMLStrings: replacementsForRelease
 }));
 
-gulp.task('default', ['build-iframe']);
+gulp.task('build-test', ['clean-test'], () => NimiqBuild.build({
+    jsEntry: 'src/keyguard.js',
+    cssEntry: 'src/keyguard.css',
+    htmlEntry: 'src/index.html',
+    rootPath: `${__dirname}/../../`,
+    distPath: 'deployment-keyguard/dist-test'
+}));
+
+gulp.task('build-test-iframe', ['build-test'], () => NimiqBuild.build({
+    jsEntry: 'src/keyguard.js',
+    cssEntry: 'src/keyguard.css',
+    htmlEntry: 'src/iframe.html',
+    rootPath: `${__dirname}/../../`,
+    distPath: 'deployment-keyguard/dist-test'
+}));
+
+gulp.task('default', ['build-iframe', 'build-test-iframe']);
